de4ebaf2a240375bb5b5f6b1d0e0f7f27c7d3307
higherstickanalytics <nknowles.bentley@gmail.com>
2025-05-12 14:29:35 -0400

    NHLAnalysis.py

--- NHLAnalysis.py
+++ NHLAnalysis.py
@@ -1,27 +1,25 @@
 import streamlit as st
 import pandas as pd
 
+# Paths to the files
+skaters_path = 'data/hockey_data/combined_skaters_hockey_game_logs.csv'
+goalies_path = 'data/hockey_data/combined_goalies_hockey_game_logs.csv'
+schedule_path = 'data/NHL_Schedule.csv'
+
+# Load the data
+skaters_df = pd.read_csv(skaters_path)
+goalies_df = pd.read_csv(goalies_path)
+schedule_df = pd.read_csv(schedule_path, parse_dates=['DATE'], dayfirst=False)
+
 # Title of the app
 st.title("Hockey Data Viewer")
 
-# Load skater data
-skaters_path = 'data/hockey_data/combined_skaters_hockey_game_logs.csv'
-skaters_df = pd.read_csv(skaters_path, parse_dates=['Date'], dayfirst=False)
-
-# Load goalie data
-goalies_path = 'data/hockey_data/combined_goalies_hockey_game_logs.csv'
-goalies_df = pd.read_csv(goalies_path, parse_dates=['Date'], dayfirst=False)
-
-# Load NHL schedule
-schedule_path = 'data/NHL_Schedule.csv'
-schedule_df = pd.read_csv(schedule_path, parse_dates=['Date'], dayfirst=False)
-
-# Display first 5 rows of each
+# Display the first 5 rows of each dataset
 st.subheader("First 5 Rows of Skaters Data")
 st.dataframe(skaters_df.head())
 
 st.subheader("First 5 Rows of Goalies Data")
 st.dataframe(goalies_df.head())
 
-st.subheader("First 5 Rows of NHL Schedule")
+st.subheader("First 5 Rows of NHL Schedule Data")
 st.dataframe(schedule_df.head())
